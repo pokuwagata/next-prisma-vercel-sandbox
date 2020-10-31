e2f8b40b99bb9b42e03e8f988a6a717eb7742ed4
pokuwagata <pokuwagata@gmail.com>
2020-10-31 15:55:29 +0900

    add ENV endpoint

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const baseUrl = dev
-  ? "http://localhost:3000"
-  : "https://yn2011-next-prisma-vercel-sandbox.vercel.app/";
+export const baseUrl = dev ? "http://localhost:3000" : process.env.END_POINT;
